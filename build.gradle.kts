9f9d57f48e56feff764f302f4ec770c88ff3f855
ahmad <ahmadtheswe@gmail.com>
2025-06-21 06:30:29 +0700

    add actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.ai:spring-ai-starter-vector-store-pgvector")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 dependencyManagement {
